4a532fd2f385cd38e8cbbb731d8e4ccc275ce18d
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2022-03-16 19:41:02 +0900

    update: enable sonala

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/g8rswimmer/go-twitter/v2"
+	"github.com/portto/solana-go-sdk/client"
+	"github.com/portto/solana-go-sdk/rpc"
+	"github.com/portto/solana-go-sdk/types"
 )
 
 var (
@@ -14,8 +19,8 @@ var (
 	FeePayerBase58 = flag.String("feepayer", "", "FeePayer no base58."+
 		" *keypair no base 58, private key no base 58 dato error deru")
 	TWITTER_CLIENT *twitter.Client
-	// SOLANA_CLIENT  *client.Client
-	// FEEPAYER       types.Account
+	SOLANA_CLIENT  *client.Client
+	FEEPAYER       types.Account
 )
 
 type authorize struct {
@@ -28,7 +33,7 @@ func (a authorize) Add(req *http.Request) {
 
 func init() {
 	flag.Parse()
-	// FEEPAYER, _ = types.AccountFromBase58(*FeePayerBase58)
+	FEEPAYER, _ = types.AccountFromBase58(*FeePayerBase58)
 
 	// twitter
 	TWITTER_CLIENT = &twitter.Client{
@@ -37,13 +42,13 @@ func init() {
 		Host:       "https://api.twitter.com",
 	}
 
-	//solana
-	// SOLANA_CLIENT = client.NewClient(rpc.DevnetRPCEndpoint)
-	// resp, err := SOLANA_CLIENT.GetVersion(context.TODO())
-	// if err != nil {
-	// 	log.Fatalf("[Solana ] Failed to version info, err: %v", err)
-	// }
-	// log.Println("[Solana ] 🎉 Solana client has launched. version", resp.SolanaCore)
+	// solana
+	SOLANA_CLIENT = client.NewClient(rpc.DevnetRPCEndpoint)
+	resp, err := SOLANA_CLIENT.GetVersion(context.TODO())
+	if err != nil {
+		log.Fatalf("[Solana]  Failed to version info, err: %v", err)
+	}
+	log.Println("[Solana]  Solana client has launched. version", resp.SolanaCore)
 }
 
 func main() {
